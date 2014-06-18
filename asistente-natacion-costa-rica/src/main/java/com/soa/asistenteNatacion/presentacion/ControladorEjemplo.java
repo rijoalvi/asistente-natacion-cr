3813dcec1408bec4c88988e2d5c10503d3fb6319
rijoalvi@gmail.com <rijoalvi@gmail.com@e3afeaac-53f4-62a1-c500-13f8f42775bc>
2014-06-18 03:17:00 +0000

    Modificados los nombres de los packages

--- asistente-natacion-costa-rica/src/main/java/com/soa/asistenteNatacion/presentacion/ControladorEjemplo.java
+++ asistente-natacion-costa-rica/src/main/java/com/soa/asistenteNatacion/presentacion/ControladorEjemplo.java
@@ -1,4 +1,4 @@
-package com.soa.asistenteNatacion.servicios;
+package com.soa.asistenteNatacion.presentacion;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
