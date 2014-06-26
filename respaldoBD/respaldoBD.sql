4713bbed864451f3134eae9f111816c41b9405d0
rijoalvi@gmail.com <rijoalvi@gmail.com@e3afeaac-53f4-62a1-c500-13f8f42775bc>
2014-06-26 02:28:56 +0000

    respaldo de la BD

--- respaldoBD/respaldoBD.sql
+++ respaldoBD/respaldoBD.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `asistente_natacion_cr` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `asistente_natacion_cr`;
--- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+-- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
 --
--- Host: localhost    Database: asistente_natacion_cr
+-- Host: 127.0.0.1    Database: asistente_natacion_cr
 -- ------------------------------------------------------
--- Server version	5.5.24-log
+-- Server version	5.6.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -53,6 +53,7 @@ DROP TABLE IF EXISTS `equipo`;
 CREATE TABLE `equipo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -64,33 +65,6 @@ CREATE TABLE `equipo` (
 LOCK TABLES `equipo` WRITE;
 /*!40000 ALTER TABLE `equipo` DISABLE KEYS */;
 /*!40000 ALTER TABLE `equipo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `nadador`
---
-
-DROP TABLE IF EXISTS `nadador`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `nadador` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) NOT NULL,
-  `apellido` varchar(45) NOT NULL,
-  `edad` int(11) NOT NULL,
-  `categoria` int(11) NOT NULL,
-  `especialidad` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `nadador`
---
-
-LOCK TABLES `nadador` WRITE;
-/*!40000 ALTER TABLE `nadador` DISABLE KEYS */;
-/*!40000 ALTER TABLE `nadador` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -132,13 +106,15 @@ DROP TABLE IF EXISTS `tiempo`;
 CREATE TABLE `tiempo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_prueba` int(11) NOT NULL,
-  `id_nadador` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   `tiempo` datetime NOT NULL,
+  `distancia_prueba` int(11) DEFAULT NULL,
+  `estilo_prueba` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `tiempo_prueba_idx` (`id_prueba`),
-  KEY `tiempo_nadador_idx` (`id_nadador`),
-  CONSTRAINT `tiempo_nadador` FOREIGN KEY (`id_nadador`) REFERENCES `nadador` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  CONSTRAINT `tiempo_prueba` FOREIGN KEY (`id_prueba`) REFERENCES `prueba` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+  KEY `tiempo_nadador_idx` (`id_usuario`),
+  CONSTRAINT `tiempo_prueba` FOREIGN KEY (`id_prueba`) REFERENCES `prueba` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `tiempo_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -167,9 +143,12 @@ CREATE TABLE `usuario` (
   `tipo` int(11) DEFAULT NULL,
   `id_nadador` int(11) DEFAULT NULL,
   `nombre` varchar(45) NOT NULL,
+  `edad` int(11) DEFAULT NULL,
+  `categoria` int(11) DEFAULT NULL,
+  `especialidad` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `usuario_nadador_idx` (`id_nadador`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -178,34 +157,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (2,'rijoalvi','88','Alvarado Villalobos','rijoalvi@gmail.com',1,0,'Ricardo');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuario_equipo`
---
-
-DROP TABLE IF EXISTS `usuario_equipo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuario_equipo` (
-  `id_equipo` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  PRIMARY KEY (`id_equipo`,`id_usuario`),
-  KEY `idUsuario_idx` (`id_usuario`),
-  CONSTRAINT `idEquipo` FOREIGN KEY (`id_equipo`) REFERENCES `equipo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `idUsuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario_equipo`
---
-
-LOCK TABLES `usuario_equipo` WRITE;
-/*!40000 ALTER TABLE `usuario_equipo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuario_equipo` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -217,4 +169,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-06-20 17:41:35
+-- Dump completed on 2014-06-25 20:28:14
